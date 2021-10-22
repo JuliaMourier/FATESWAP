6fccea768dab58efb5d17cfc17a425f5a90f2c20
PabloMsnv <pabmaiso@live.fr>
2021-10-21 21:52:41 -0400

    Proto menu pause

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Restart() {
+        Debug.Log("Restart...");
+    }
+
+    public void LoadSettings() {
+        Debug.Log("Load Settings...");
+    }
+
+    public void LoadMenu() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+}
