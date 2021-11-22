5822409409fff78286a3e237f291df80938c927e
OctoJul <julia.mourier@isen.yncrea.fr>
2021-11-21 23:38:49 -0500

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/Enemies/OrderRobot.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderRobot : MonoBehaviour
+{
+    public List<Robot> listOfRobots;
+
+    public Collectables key;
+
+    public Switch hiddenSwitch;
+
+    private int check = 0;
+
+    private void Update() {
+        if(check == listOfRobots.Count){
+            key.gameObject.SetActive(true);
+            hiddenSwitch.gameObject.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+        
+        for(int i = check; i < listOfRobots.Count; i++){
+            if(listOfRobots[i].isAlive == false){
+                if(i > check + 1){
+                    check = 0;
+                    FindObjectOfType<GameManager>().Restart();
+                }
+            }
+        }
+
+        if(listOfRobots[check].isAlive == false){
+            check++;
+        }
+    }
+
+}
