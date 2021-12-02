e0aeea188e0da5c8e69b5b27730cebd7acc74880
OctoJul <julia.mourier@isen.yncrea.fr>
2021-12-02 16:13:07 -0500

    Link level menu to Dialog (not to level)
    + DoorToBoss => if solo join the right room

--- Assets/Scripts/Environment/DoorToBoss.cs
+++ Assets/Scripts/Environment/DoorToBoss.cs
@@ -6,7 +6,12 @@ using UnityEngine.SceneManagement;
 public class DoorToBoss : Door
 {
     protected override void OnDoorEntered() 
-    {  
-        SceneManager.LoadScene("DialogBoss"); //Load the scene of dialog of the boss scene
+    {   
+        if(FindObjectOfType<GameManager>().solo){
+            SceneManager.LoadScene("DialogBoss_Solo"); //Load the scene of dialog of the boss scene for solo mode
+        }
+        else{
+            SceneManager.LoadScene("DialogBoss"); //Load the scene of dialog of the boss scene
+        }
     }
 }
