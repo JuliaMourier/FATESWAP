4fc1982d170ee64fc189133be7691a36aee6425b
OctoJul <julia.mourier@isen.yncrea.fr>
2021-12-02 14:24:30 -0500

    Scenes Organisation folders

--- Assets/Scripts/Characters/Fei.cs
+++ Assets/Scripts/Characters/Fei.cs
@@ -6,11 +6,10 @@ public class Fei : Character
 {
     // Decrease the size of the BoxCollider of Fei when he's small
     public override void OnPowerActivate()
-    {
+    {        
         base.OnPowerActivate();
         GetComponent<BoxCollider2D>().size = new Vector2(0.37f, 0.4f);
         GetComponent<BoxCollider2D>().offset = new Vector2(0.1f, -0.2f);
-
     }
 
     // Increase the size of the BoxCollider of Fei when he's small
@@ -19,7 +18,5 @@ public class Fei : Character
         base.OnPowerDeactivate();
         GetComponent<BoxCollider2D>().size = new Vector2(0.6f, 0.66f);
         GetComponent<BoxCollider2D>().offset = new Vector2(0f, 0f);
-
-
     }
 }
--- Assets/Scripts/MultiplayerMode/Spawner.cs
+++ Assets/Scripts/MultiplayerMode/Spawner.cs
@@ -10,8 +10,9 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     public void Start()
     {
+        int index = (int) PhotonNetwork.LocalPlayer.CustomProperties["number"];
         Debug.Log(PhotonNetwork.LocalPlayer.CustomProperties["number"]);
-        PhotonNetwork.Instantiate(playerPrefabs[0].name, charactersPosition[0].position, Quaternion.identity);
+        PhotonNetwork.Instantiate(playerPrefabs[index].name, charactersPosition[index].position, Quaternion.identity);
 /*
         if(GameObject.Find("Luciem(Clone)") == null){
             PhotonNetwork.Instantiate(playerPrefabs[0].name, charactersPosition[0].position, Quaternion.identity);
